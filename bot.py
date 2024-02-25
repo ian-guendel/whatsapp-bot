5b4f7fe5e47195c3c70345e3f5996428a813ebec
Ian Guendel <ian.guendelh@gmail.com>
2024-02-25 16:10:22 -0600

    General Update

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import pywhatkit as kit
 import time
 total_messages = range(4)
-phone = "phone_number"
-group_id = "group_id"
-image_path = "image_local_path"
+phone = ""
+group_id = ""
+image_path = ""
+image_caption = ""
 
 try:
     for msg in total_messages:
-        kit.sendwhats_image(group_id,image_path,"Ya esta el video!",10,True,1)
+        kit.sendwhats_image(phone,image_path,image_caption,10,True,5)
         print(f'Message {str(msg)} of {str(5)}')
         time.sleep(2)
 except Exception as ex:
+
     print(ex)
 
