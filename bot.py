02264be9a1c5d618496a4f15666686fe325052fe
Ian Guendel <ian.guendelh@gmail.com>
2024-02-25 13:49:09 -0600

    Script Update

--- bot.py
+++ bot.py
@@ -1,19 +1,19 @@
 import pywhatkit as kit
 import time
-import datetime
-import random
-import pyautogui as pg
 import time
-import keyboard as k
+
+
 # To schedule messages
 # kit.sendwhatmsg('+50688138143',"Bot message")
 
 
-print('Starting to send the message...')
-# To send instant messages
+# print('Starting to send the message...')
+# # To send instant messages
 try:
-    kit.sendwhatmsg_instantly('+50688138143',"Guapa",9,True,1)
+    # kit.sendwhatmsg_instantly('+50687133953',"Guapa",9,True,1)
+    kit.sendwhats_image('+50687133953',"/Users/ianguendel/Documents/GitHub/whatsapp-bot/gif1.gif","Los acordes... :P",10,True,2)
     print('message send!')
 
 except Exception as ex:
     print(ex)
+
